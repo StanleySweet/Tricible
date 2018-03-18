47c93d9651d482c88565fff378485d00bcf43928
Stanislas Dolcini <stanislas.dolcini@devinci.fr>
2018-03-18 23:28:51 +0100

    Improve sonar support

--- file.bat
+++ file.bat
@@ -1,4 +1,5 @@
 cd E:\Dev\Tricible
 "C:\PROGRAM FILES (X86)\MICROSOFT VISUAL STUDIO\2017\ENTERPRISE\COMMON7\IDE\COMMONEXTENSIONS\MICROSOFT\CMAKE\CMake\bin\cmake.exe" CMakeLists.txt 
 build-wrapper-win-x86-64.exe --out-dir bw_output "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Tricible.sln /p:Configuration=Release
+sonar-scanner
 cd E:\Dev\Tricible
